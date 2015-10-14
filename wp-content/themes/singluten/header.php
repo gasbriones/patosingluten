5dd0c582e96a2f82f88091fcdab1d3b352e64cd0
Gaston Briones <mauricio_briones@artear.com.ar>
2015-10-14 18:29:16 -0300

    fix

--- wp-content/themes/singluten/header.php
+++ wp-content/themes/singluten/header.php
@@ -9,34 +9,33 @@
  * @since SinGluten 1.0
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?> >
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width">
-    <meta content="Arte pop, latinoamérica-Arte, pop, Argentina-Art pop, Argentina-Arte pop, Salta-Art pop, Salta-Pintura pop, Argentina-Pintura pop, Buenos Aires-Pintura pop, Salta-Cuadros pop, Argentina-Dibujos pop, Argentina-artista, plástico, Argentina-artista plástico, Buenos Aires-artista, plástico Salta-artista, visual, Argentina-artista visual, Buenos Aires-Artista visual, Salta-pop, drawing-pop, painting-Arte, contemporaneo, Argentina-Arte contemporaneo, Salta-Estilo, pop-Serie, Pop-Muestra, pop-Exposición, arte, pop-Mural, pop-Mural Buenos Aires-colores, saturados-dibujo, oriental-oriental, drawing-dibujo, zen-Dibujo, geisha-objetos, intervenidos, pop-Contraste-arte, mujer-arte, imagen mujer-arte, extravagante-arte, geisha" name="keywords">
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="description" content="<?php bloginfo( 'description'); ?>" />
 
-    <meta content="Descripcion" name="description">
     <!-- Facebook -->
     <meta name="og:type" content="website"/>
-    <meta name="og:title" content="Francisco Speicher"/>
-    <meta name="og:description" content="Descripcion"/>
-    <meta name="og:url" content="url"/>
-    <meta content="Francisco Speicher" name="og:site_name">
-    <meta content="Buenos Aires" name="og:region">
-    <meta content="Argentina" name="og:country-name">
+    <meta name="og:title" content="<?php bloginfo('name'); ?>"/>
+    <meta name="og:description" content="<?php bloginfo('description'); ?>"/>
+    <meta name="og:url" content="<?php echo get_site_url() ?>"/>
+    <meta name="og:site_name" content="<?php bloginfo('name'); ?>" />
+    <meta name="og:region" content="Buenos Aires" />
+    <meta name="og:country-name" content="Argentina" />
     <!-- twitter -->
     <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:url" content="url"/>
-    <meta name="twitter:title" content="Francisco Speicher"/>
-    <meta name="twitter:description" content="Descripcion"/>
-    <meta content="@franspeicher" name="twitter:site">
-    <meta content="@gasbriones" name="twitter:creator">
+    <meta name="twitter:url" content="<?php echo get_site_url() ?>"/>
+    <meta name="twitter:title" content="<?php bloginfo('name'); ?> "/>
+    <meta name="twitter:description" content="<?php bloginfo('description'); ?>"/>
+    <meta name="twitter:site" content="@patosingluten" />
+    <meta name="twitter:creator" content="@gasbriones" />
     <!-- Links -->
 
     <link href="<?php echo get_template_directory_uri(); ?>/css/normalize.css" rel="stylesheet" type="text/css">
     <link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet" type="text/css">
     <link href="<?php echo get_template_directory_uri(); ?>/css/mediaqueries.css" rel="stylesheet" type="text/css">
-    <title><?php bloginfo('name'); ?></title>
+    <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?> </title>
     <?php wp_head(); ?>
 </head>
 
