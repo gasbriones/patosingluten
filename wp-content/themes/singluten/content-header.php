aed23a298740815e8cc08a476778ca9a7c3f9ec4
Gaston Briones <mauricio_briones@artear.com.ar>
2015-10-15 18:30:16 -0300

    fix

--- wp-content/themes/singluten/content-header.php
+++ wp-content/themes/singluten/content-header.php
@@ -3,6 +3,8 @@
         <a href="<?php echo get_site_url();?>" class="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png"></a>
     </div>
     <ul class="social">
-
+        <li>FB</li>
+        <li>TW</li>
+        <li>INS</li>
     </ul>
 </header>
