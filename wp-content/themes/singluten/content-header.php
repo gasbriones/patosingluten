af59b10db874add5e8c9e99073d07bc669a47c39
Gaston Briones <gbriones@cmd.com.ar>
2015-10-15 23:35:56 -0300

    fix

--- wp-content/themes/singluten/content-header.php
+++ wp-content/themes/singluten/content-header.php
@@ -1,4 +1,11 @@
 <header id="header" class="block wrapper clearfix">
+    <div class="search">
+        <form action="/sarch">
+            <input type="text" placeholder="buscar recetas" value=""/>
+            <input type="submit" src="<?php echo get_site_url();?>/images/lupa.png">
+        </form>
+
+    </div>
     <div class="logo-wrapper center">
         <a href="<?php echo get_site_url();?>" class="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png"></a>
     </div>
@@ -6,5 +13,11 @@
         <li>FB</li>
         <li>TW</li>
         <li>INS</li>
+    </ul>
+    <ul class="main-menu">
+        <li><a href="#">Sobre Pato</a></li>
+        <li><a href="#">Recetas</a></li>
+        <li><a href="#">Novedades</a></li>
+        <li><a href="#">Contacto</a></li>
     </ul>
 </header>
