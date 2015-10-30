21ffd8504a40c41545ffb263e3d53f05b25358f2
Gaston Briones <mauricio_briones@artear.com.ar>
2015-10-30 18:14:11 -0300

    fix

--- wp-content/themes/singluten/sobre-pato.php
+++ wp-content/themes/singluten/sobre-pato.php
@@ -7,26 +7,38 @@
 
 ?>
 <?php while (have_posts()) : the_post(); ?>
-    <h4 class="page-title">
-        <?php the_title(); ?>
-    </h4>
-    <figure class="pic-one">
-        <img src="<?php echo the_field('sobre_pato_imagen_1') ?>">
-    </figure>
-    <figure class="pic-two">
-        <img src="<?php echo the_field('sobre_pato_imagen_2') ?>">
-    </figure>
-    <ul class="social">
-        <li><a href="<?php echo $fb; ?>" class="socicon socicon-facebook "><span class="fb-url">/<?php echo basename(parse_url($fb, PHP_URL_PATH)); ?></span></a></li>
-        <li><a href="<?php echo $tw; ?>" class="socicon socicon-twitter"><span class="tw-url">@<?php echo basename(parse_url($tw, PHP_URL_PATH)); ?></span></a></li>
-        <li><a href="<?php echo $inst; ?>" class="socicon socicon-instagram"><span class="inst-url"><?php echo basename(parse_url($inst, PHP_URL_PATH)); ?></span></a></li>
-    </ul>
+    <div class="about-pato">
+        <h4 class="page-title">
+            <?php the_title(); ?>
+        </h4>
+        <figure class="pic-one">
+            <img src="<?php echo the_field('sobre_pato_imagen_1') ?>">
+        </figure>
+        <figure class="pic-two">
+            <img src="<?php echo the_field('sobre_pato_imagen_2') ?>">
+        </figure>
+        <ul class="social">
+            <li><a href="<?php echo $fb; ?>" class="socicon socicon-facebook "><span class="fb-url"><i>/</i><?php echo basename(parse_url($fb, PHP_URL_PATH)); ?></span></a></li>
+            <li><a href="<?php echo $tw; ?>" class="socicon socicon-twitter"><span class="tw-url"><i>@</i><?php echo basename(parse_url($tw, PHP_URL_PATH)); ?></span></a></li>
+            <li><a href="<?php echo $inst; ?>" class="socicon socicon-instagram"><span class="inst-url"><i>/</i><?php echo basename(parse_url($inst, PHP_URL_PATH)); ?></span></a></li>
+        </ul>
 
-    <div class="white-board">
-        <article >
-            <?php the_content(); ?>
+        <div class="white-board">
+            <div class="decoration-pic-1"></div>
+            <div class="decoration-pic-2"></div>
+            <div class="decoration-pic-3"></div>
+            <article>
+                <?php the_content(); ?>
+            </article>
+        </div>
+    </div>
+
+    <div class="desire">
+        <article>
+            <?php echo the_field('sobre_pato_deseo') ?>
         </article>
     </div>
+
 
 
 <?php endwhile;?>
