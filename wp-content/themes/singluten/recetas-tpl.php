97f6e6a3076e0cac5450474d905b4da6b8dde338
Gaston Briones <mauricio_briones@artear.com.ar>
2015-11-17 18:33:35 -0300

    fix

--- wp-content/themes/singluten/recetas-tpl.php
+++ wp-content/themes/singluten/recetas-tpl.php
@@ -28,7 +28,7 @@ switch ($post_id) {
         break;
 }
 
-$paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+$paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
 $args = array(
     'cat' => 28,
     'tag' => $tag,
@@ -38,7 +38,6 @@ $args = array(
 );
 
 $query = new WP_Query($args);
-
 
 $args_menu = array(
     'cat' => 28,
@@ -57,54 +56,43 @@ $menu_query = new WP_Query($args_menu);
     <div id="main" class="wrapper">
         <section class="block recipes-view clearfix">
             <h4 class="page-title wow bounceInDown"><?php echo get_the_title(); ?></h4>
-            <a class="back-to-site hvr-float-shadow" href="<?php echo site_url()."#recetas" ; ?>">Volver al <span>home</span></a>
+            <a class="back-to-site hvr-float-shadow" href="<?php echo site_url(); ?>">Volver al <span>home</span></a>
+
+            <div class="main-menu margin center">
+                <?php
+                $cleaner_menu = wp_nav_menu( array( 'theme_location' => 'category-menu','menu_class' => 'menu category-nav clearfix'));
+                ?>
+            <div>
 
             <aside>
                 <ul class="recipes-list">
                     <?php
                     if ($menu_query->have_posts()):
-                        $i=0;
+                        $i = 0;
                         while ($menu_query->have_posts()):$menu_query->the_post();
                             ?>
                             <li>
-                                <a  class="<?php echo $post->ID == $get_post_id || ($get_post_id == '' && $i== 0) ? 'active' : ''; ?>" href="<?php echo post_permalink($post_id) . "?receta=" . $post->ID ?>"><?php the_title(); ?></a>
+                                <a class="<?php echo $post->ID == $get_post_id || ($get_post_id == '' && $i == 0) ? 'active' : ''; ?>"
+                                   href="<?php echo post_permalink($post_id) . "?receta=" . $post->ID ?>"><?php the_title(); ?></a>
                             </li>
-                        <?php $i++; endwhile;
+                            <?php $i++; endwhile;
 
                     endif;
                     ?>
                 </ul>
                 <div class="paginator">
-                <?php
+                    <?php
                     $big = 999999999; // need an unlikely integer
 
-                    echo paginate_links( array(
-                        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                    echo paginate_links(array(
+                        'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
                         'format' => '?paged=%#%',
-                        'current' => max( 1, get_query_var('paged') ),
+                        'current' => max(1, get_query_var('paged')),
                         'total' => $menu_query->max_num_pages,
-                        'prev_next'=> false,
-                    ) );
-                ?>
+                        'prev_next' => false,
+                    ));
+                    ?>
                 </div>
-
-
-                <?php
-                if($menu_query->max_num_pages > 1){?>
-                    <p class="navrechts">
-                        <?php
-                        if ($paged > 1) { ?>
-                            <a href="<?php echo '?paged=' . ($paged -1); //prev link ?>"><</a>
-                        <?php }
-                        for($i=1;$i<=$loopb->max_num_pages;$i++){?>
-                            <a href="<?php echo '?paged=' . $i; ?>" <?php echo ($paged==$i)? 'class="selected"':'';?>><?php echo $i;?></a>
-                        <?php
-                        }
-                        if($paged < $loopb->max_num_pages){?>
-                            <a href="<?php echo '?paged=' . ($paged + 1); //next link ?>">></a>
-                        <?php } ?>
-                    </p>
-                <?php } ?>
 
             </aside>
             <div class="white-board">
@@ -113,27 +101,28 @@ $menu_query = new WP_Query($args_menu);
                     $query = new WP_Query(array('p' => $get_post_id));
                 }
                 if ($query->have_posts()):
-                    while ($query->have_posts()): $query->the_post();?>
-                    <?php the_title('<h3 class="title">', '</h3>'); ?>
-                    <article>
-                        <?php if (get_field('recetas_imagen')):?>
-                            <div class="recipe-img"><img src="<?php echo the_field('recetas_imagen') ?>" /></div>
-                        <?php endif; ?>
+                while ($query->have_posts()):
+                $query->the_post();?>
+                <?php the_title('<h3 class="title">', '</h3>'); ?>
+                <article>
+                    <?php if (get_field('recetas_imagen')): ?>
+                        <div class="recipe-img"><img src="<?php echo the_field('recetas_imagen') ?>"/></div>
+                    <?php endif; ?>
 
-                        <h3 class="sub-title">Ingredientes</h3>
-                        <?php the_content(); ?>
+                    <h3 class="sub-title">Ingredientes</h3>
+                    <?php the_content(); ?>
 
-                        <?php if (get_field('recetas_procedimiento')):?>
-                            <h3 class="sub-title">Procedimiento</h3>
-                            <p><?php echo the_field('recetas_procedimiento') ?></p>
-                        <?php endif; ?>
+                    <?php if (get_field('recetas_procedimiento')): ?>
+                        <h3 class="sub-title">Procedimiento</h3>
+                        <p><?php echo the_field('recetas_procedimiento') ?></p>
+                    <?php endif; ?>
 
-                        <?php if (get_field('recetas_nota')):?>
-                            <h3 class="sub-title">Nota</h3>
-                            <p><?php echo the_field('recetas_nota') ?></p>
-                        <?php endif; ?>
+                    <?php if (get_field('recetas_nota')): ?>
+                        <h3 class="sub-title">Nota</h3>
+                        <p><?php echo the_field('recetas_nota') ?></p>
+                    <?php endif; ?>
 
-                        <?php endwhile;
+                    <?php endwhile;
                     else:
                         echo '<span class="error">No se encontraron recetas</span>';
                     endif;
