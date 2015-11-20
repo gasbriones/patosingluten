f499114513d22a57dcd5f061086571dd20eed8e1
Gaston Briones <gbriones@cmd.com.ar>
2015-11-20 17:52:57 -0300

    fix

--- wp-content/themes/singluten/footer.php
+++ wp-content/themes/singluten/footer.php
@@ -1,14 +1,25 @@
+<?php
+query_posts('page_id=6');
+$fb = get_field('facebook', 'user_2');
+$tw = get_field('twitter', 'user_2');
+$inst = get_field('instagram', 'user_2');
+
+?>
+
 <footer id="footer" class="clearfix">
     <section id="contact" class="wrapper contact">
         <h4 class="page-title wow bounceInDown">
             Contacto
         </h4>
-
+        <div class="decoration-1"></div>
+        <ul class="social wow fadeIn">
+            <li><a href="<?php echo $fb; ?>" class="socicon socicon-facebook "><span class="fb-url"><i>/</i><?php echo basename(parse_url($fb, PHP_URL_PATH)); ?></span></a></li>
+            <li><a href="<?php echo $tw; ?>" class="socicon socicon-twitter"><span class="tw-url"><i>@</i><?php echo basename(parse_url($tw, PHP_URL_PATH)); ?></span></a></li>
+            <li><a href="<?php echo $inst; ?>" class="socicon socicon-instagram"><span class="inst-url"><i>/</i><?php echo basename(parse_url($inst, PHP_URL_PATH)); ?></span></a></li>
+        </ul>
         <div class="margin center">
-
             <div class="contact-board">
                 <h3 class="title">Newsletter</h3>
-
                 <form>
                     <div>
                         <label>Nombre</label>
@@ -32,8 +43,6 @@
                     </div>
                 </form>
             </div>
-        </div>
-
-        </div>
     </section>
+    <?php include 'plinth.php' ?>
 </footer>
